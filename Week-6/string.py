e4c3a7a4f3514f2a082136897f670713182d96eb
vishalyadav28148 <vishalyadavkanuganti28@gmail.com>
2025-05-01 12:09:43 +0530

    Create string.py

---
+++ Week-6/string.py
@@ -0,0 +1,34 @@
+my_string = "Vishal,Yadav"
+print(my_string[0:6])
+print(my_string[7:])
+print(my_string[:6])
+print(my_string[-5:])
+length = len(my_string)
+my_string = "vishalyadav"
+print(length)
+string1="Vishal"
+string2="Yadav"
+concatenated_string = string1 + ' ' + string2
+print(concatenated_string)
+text = "python lab"
+print(text.upper())
+text = "PYTHON LAB"
+print(text.lower())
+text = "  ACE ENGINEERING COLLEGE  "
+print(text.strip())
+text = "COMPUTER LAB"
+print(text.split())
+text = "COMPUTER LAB"
+print(text.split(","))
+text = "vishal,yadav"
+print(text.replace("yadav","tillu"))
+text = "vishal yadav"
+print(text.find("yadav"))
+print(text.find("nikhil"))
+my_string = "Fire,Fox!"
+print(my_string.lower())
+print(my_string.upper())
+print(my_string.strip())
+print(my_string.split(','))
+print(my_string.replace("Fir","Box"))
+print(my_string.find("Fire"))
